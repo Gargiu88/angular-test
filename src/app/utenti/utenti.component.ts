22f2766af21559ada36df01f1b59d65fb63ae051
lenovo <v.rampielli@greenvulcano.com>
2021-11-11 12:05:32 +0100

    funzione cancella

--- src/app/utenti/utenti.component.ts
+++ src/app/utenti/utenti.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./utenti.component.css']
 })
 export class UtentiComponent implements OnInit {
+  username : string;
+  email: string;
+  option: string;
+  referente: string;
+  result:  string;
+  results: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  btnAdd(){
+  switch (this.option){
+    case 'referente1':
+    this.result = this.username + " " + this.email + " referente1 ";
+    break;
+    case 'referente2': 
+    this.result = this.username + " " + this.email + " referente2 "; 
+    break;
+    case 'referente3': 
+    this.result = this.username + " " + this.email + " referente3 "; 
+    break;
+    case 'referente4': 
+    this.result = this.username + " " + this.email + " referente4 "; 
+    break;
+    case 'referente5': 
+    this.result = this.username + " " +  this.email  + " referente5 "; 
+    break;
+  }
+  this.results.push(this.result);
+
+}
+
+ btnCanc(res){
+ this.results.splice(res,1);
+ // this.results.splice(this.results.indexOf(res),1)
+  // delete this.result;
+ }
+
+ validateEmail(email) {
+  const regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regularExpression.test(String(email).toLowerCase());
+ }
 }
